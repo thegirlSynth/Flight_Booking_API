dece448ea2bbe37b45b3e87c105d18b761f9454e
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2024-01-12 03:14:57 +0100

    Update API models

---
+++ radar/app/models.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+"""
+
+from app import db
+from flask_user import UserMixin
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    roles = db.relationship(
+        "Role", secondary="user_roles", backref=db.backref("users", lazy="dynamic")
+    )
+    bookings = db.relationship("Booking", backref="user", lazy=True)
+
+
+class Role(db.Model):
+    __tablename__ = "roles"
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+
+
+class UserRoles(db.Model):
+    __tablename__ = "user_roles"
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey("users.id", ondelete="CASCADE"))
+    role_id = db.Column(db.Integer(), db.ForeignKey("roles.id", ondelete="CASCADE"))
+
+
+class Booking(db.Model):
+    __tablename__ = "bookings"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    flight_id = db.Column(db.Integer, db.ForeignKey("flights.id"), nullable=False)
+
+
+class Flight(db.Model):
+    __tablename__ = "flights"
+    id = db.Column(db.Integer, primary_key=True)
