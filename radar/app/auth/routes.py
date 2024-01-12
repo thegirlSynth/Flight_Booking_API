97b9e3b50c3808731d7ff98bc670a07760bd06ae
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2024-01-12 03:11:56 +0100

    Add endpoints for user authentication

---
+++ radar/app/auth/routes.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""
+"""
+
+import bcrypt
+from flask import Blueprint, jsonify, request, redirect, url_for
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+from app.models import User, Role
+from app import db
+
+auth = Blueprint("auth", __name__)
+
+@auth.route("/")
+def index() -> str:
+    return jsonify({"message": "Welcome to the Flight Booking API"})
+
+@auth.route("/home")
+def home():
+    if current_user.is_authenticated:
+        return jsonify({"message": f"Welcome to the Home Page, {current_user.name}"})
+    else:
+        return redirect(url_for("auth.login")), 401
+
+@auth.route("/signup", methods=["POST"], strict_slashes=False)
+def signup():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    name = request.form.get("name")
+
+    existing_user = User.query.filter_by(email=email).first()
+    if existing_user:
+        return jsonify({"message": "Email already registered"}), 400
+
+    password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+    new_user = User(email=email, name=name, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "User created successfully"}), 201
+
+
+@auth.route("/login", methods=["POST"], strict_slashes=False)
+def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    user = User.query.filter_by(email=email).first()
+    if not user or not bcrypt.checkpw(password.encode("utf-8"), user.password):
+        return jsonify({"message": "Invalid credentials"}), 401
+
+    login_user(user)
+    #return redirect(url_for("auth.home")), 200
+    return jsonify({"message": "Logged in successfully"}), 200
+
+
+@auth.route("/logout", strict_slashes=False)
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logged out successfully"}), 200
