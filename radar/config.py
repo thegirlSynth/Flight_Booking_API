7344741bd3976de121aba5ad86a041c0499fb36b
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2024-01-14 12:45:28 +0100

    Replace Flask User functionalities with FLask Login

--- radar/config.py
+++ radar/config.py
@@ -10,7 +10,7 @@ class ConfigClass(object):
     """Flask application config"""
 
     # Flask settings
-    SECRET_KEY = os.getenv("SECRET_KEY", "my_secret_key")
+    SECRET_KEY = os.getenv("SECRET_KEY", "my_secret_key_is_my_secret")
 
     # Flask-SQLAlchemy settings
     SQLALCHEMY_DATABASE_URI = "sqlite:///radar.db"
@@ -26,8 +26,8 @@ class ConfigClass(object):
     MAIL_DEFAULT_SENDER = '"MyApp" <noreply@radar.com>'
 
     # Flask-User settings
-    USER_APP_NAME = "Flask-User Basic App"
-    USER_ENABLE_EMAIL = True
-    USER_ENABLE_USERNAME = False
-    USER_EMAIL_SENDER_NAME = USER_APP_NAME
-    USER_EMAIL_SENDER_EMAIL = "noreply@eradar.com"
+    # USER_APP_NAME = "Flask-User Basic App"
+    # USER_ENABLE_EMAIL = True
+    # USER_ENABLE_USERNAME = False
+    # USER_EMAIL_SENDER_NAME = USER_APP_NAME
+    # USER_EMAIL_SENDER_EMAIL = "noreply@eradar.com"
