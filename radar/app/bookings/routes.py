ea0e5f7783368439fcec9ddd218c294d7b946a25
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2024-01-14 12:42:38 +0100

    Correct request format

--- radar/app/bookings/routes.py
+++ radar/app/bookings/routes.py
@@ -3,27 +3,65 @@
 """
 
 from flask import Blueprint, jsonify, request
-from flask_user import login_required
+from flask_login import login_required, current_user
 from app.models import Booking, Flight
 from app import db
 
 bookings = Blueprint("bookings", __name__)
 
 
-@bookings.route("/book-flight", methods=["POST"])
+@bookings.route("/search-flights", methods=["POST"], strict_slashes=False)
+def search_flights():
+    source = request.form.get("source")
+    destination = request.form.get("destination")
+    departure_date = request.form.get("departure_date")
+
+    query = Flight.query
+
+    if source:
+        query = query.filter_by(source=source)
+    if destination:
+        query = query.filter_by(destination=destination)
+    if departure_date:
+        query = query.filter_by(departure_date=departure_date)
+
+    flights = query.all()
+    flights_data = [
+        {
+            "id": flight.id,
+            "source": flight.source,
+            "destination": flight.destination,
+            "departure_date": flight.departure_date,
+        }
+        for flight in flights
+    ]
+
+    return jsonify({"flights": flights_data})
+
+
+@bookings.route("/book-flight", methods=["POST"], strict_slashes=False)
 @login_required
 def book_flight():
     user_id = current_user.id
-    flight_id = request.json.get("flight_id")
+    flight_id = request.form.get("flight_id")
 
     new_booking = Booking(user_id=user_id, flight_id=flight_id)
     db.session.add(new_booking)
     db.session.commit()
 
-    return jsonify({"message": "Flight booked successfully"}), 201
+    return (
+        jsonify(
+            {
+                "message": f"Flight booked successfully\n This is your booking ID: {new_booking.id}"
+            }
+        ),
+        201,
+    )
 
 
-@bookings.route("/cancel-booking/<int:booking_id>", methods=["DELETE"])
+@bookings.route(
+    "/cancel-booking/<int:booking_id>", methods=["DELETE"], strict_slashes=False
+)
 @login_required
 def cancel_booking(booking_id):
     booking = Booking.query.get(booking_id)
